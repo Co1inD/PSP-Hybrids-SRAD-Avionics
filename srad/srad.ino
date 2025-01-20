367eddb6da779c5bf1a51d814a5c40adf879352c
treecaptcha <treecaptcha@treecaptcha.net>
2025-01-20 15:06:57 -0500

    GPIO Assignments

--- srad/srad.ino
+++ srad/srad.ino
@@ -1,4 +1,32 @@
 
+// GPIO ASSN
+#define FIRE_1 1
+#define PYRO_SENSE1 2
+#define FIRE_2 3
+#define PYRO_SENSE2 4
+#define BARO_SCLK 5
+#define BARO_SDI 6
+#define BARO_SDO 7
+#define BARO_CS 8
+#define BNO_SCLK 9
+#define BNO_SDA 10
+#define BNO_INT 11
+#define BNO_NRESET 12
+#define FLASH_CLK 13
+#define FLASH_CS 14
+#define FLASH_DO 15 
+#define FLASH_DI 16
+#define ESP_USB_N 19
+#define ESP_USB_P 20
+#define GPS_EXTINT 47
+#define Flash_CLK 34
+#define Flash_CS 35
+#define Flash_MISO 36
+#define Flash_MOSI 37
+#define GPS_EXTINT 42
+#define GPS_SCLK 45
+#define GPS_SDA 46
+#define Buzzer 38
 
 
 
