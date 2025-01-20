05e6b7c71e90e246be97099f59283d9a45317d9e
treecaptcha <treecaptcha@treecaptcha.net>
2025-01-20 15:49:13 -0500

    Kalman filter code

--- srad/srad.ino
+++ srad/srad.ino
@@ -32,10 +32,171 @@
 
 
 void setup() {
+  // GPS
+
+  // Barometer
+
+  // IMU
+
+  // Hi G
+
   
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 
+}
+
+
+
+// Kalman filter code
+// don't ask questions you don't need the answers to
+
+// State variables
+float x[6] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0}; // [x, y, z, vx, vy, vz]
+
+// Matrices
+float F[6][6] = {
+  {1, 0, 0, 0.01, 0, 0}, {0, 1, 0, 0, 0.01, 0}, {0, 0, 1, 0, 0, 0.01},
+  {0, 0, 0, 1, 0, 0},    {0, 0, 0, 0, 1, 0},    {0, 0, 0, 0, 0, 1}
+};
+
+float B[6][3] = {
+  {0.00005, 0, 0}, {0, 0.00005, 0}, {0, 0, 0.00005},
+  {0.01, 0, 0},    {0, 0.01, 0},    {0, 0, 0.01}
+};
+
+float H[3][6] = {
+  {0, 0, 0, 1, 0, 0}, {0, 0, 0, 0, 1, 0}, {0, 0, 0, 0, 0, 1}
+};
+
+float P[6][6] = {0}; // Initialize state covariance to zero
+float Q[6][6] = {0}; // Process noise covariance
+float R[3][3] = { // Measurement noise covariance
+  {0.1, 0, 0}, {0, 0.1, 0}, {0, 0, 0.1}
+};
+
+float K[6][3]; // Kalman gain
+
+void kalmanUpdate(float z[3]) {
+  // Compute Kalman gain: K = P * H^T / (H * P * H^T + R)
+  float PHt[6][3] = {0};
+  for (int i = 0; i < 6; i++) {
+    for (int j = 0; j < 3; j++) {
+      for (int k = 0; k < 6; k++) {
+        PHt[i][j] += P[i][k] * H[j][k];
+      }
+    }
+  }
+
+  float S[3][3] = {0};
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      for (int k = 0; k < 6; k++) {
+        S[i][j] += H[i][k] * PHt[k][j];
+      }
+      S[i][j] += R[i][j];
+    }
+  }
+
+  for (int i = 0; i < 6; i++) {
+    for (int j = 0; j < 3; j++) {
+      K[i][j] = PHt[i][j] / S[j][j];
+    }
+  }
+
+  // Update state: x = x + K * (z - H * x)
+  float y[3] = {0};
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 6; j++) {
+      y[i] -= H[i][j] * x[j];
+    }
+    y[i] += z[i];
+  }
+
+  for (int i = 0; i < 6; i++) {
+    for (int j = 0; j < 3; j++) {
+      x[i] += K[i][j] * y[j];
+    }
+  }
+
+  // Update covariance: P = (I - K * H) * P
+  float KH[6][6] = {0};
+  for (int i = 0; i < 6; i++) {
+    for (int j = 0; j < 6; j++) {
+      for (int k = 0; k < 3; k++) {
+        KH[i][j] += K[i][k] * H[k][j];
+      }
+    }
+  }
+
+  float I_KH[6][6] = {0};
+  for (int i = 0; i < 6; i++) {
+    I_KH[i][i] = 1.0;
+    for (int j = 0; j < 6; j++) {
+      I_KH[i][j] -= KH[i][j];
+    }
+  }
+
+  float newP[6][6] = {0};
+  for (int i = 0; i < 6; i++) {
+    for (int j = 0; j < 6; j++) {
+      for (int k = 0; k < 6; k++) {
+        newP[i][j] += I_KH[i][k] * P[k][j];
+      }
+    }
+  }
+
+  for (int i = 0; i < 6; i++) {
+    for (int j = 0; j < 6; j++) {
+      P[i][j] = newP[i][j];
+    }
+  }
+}
+
+void kalmanPredict(float accel[3]) {
+  // Predict state: x = F * x + B * u
+  float Fx[6] = {0};
+  for (int i = 0; i < 6; i++) {
+    for (int j = 0; j < 6; j++) {
+      Fx[i] += F[i][j] * x[j];
+    }
+  }
+
+  float Bu[6] = {0};
+  for (int i = 0; i < 6; i++) {
+    for (int j = 0; j < 3; j++) {
+      Bu[i] += B[i][j] * accel[j];
+    }
+  }
+
+  for (int i = 0; i < 6; i++) {
+    x[i] = Fx[i] + Bu[i];
+  }
+
+  // Predict covariance: P = F * P * F^T + Q
+  float FP[6][6] = {0};
+  for (int i = 0; i < 6; i++) {
+    for (int j = 0; j < 6; j++) {
+      for (int k = 0; k < 6; k++) {
+        FP[i][j] += F[i][k] * P[k][j];
+      }
+    }
+  }
+
+  float FPFt[6][6] = {0};
+  for (int i = 0; i < 6; i++) {
+    for (int j = 0; j < 6; j++) {
+      for (int k = 0; k < 6; k++) {
+        FPFt[i][j] += FP[i][k] * F[j][k];
+      }
+    }
+  }
+
+  for (int i = 0; i < 6; i++) {
+    for (int j = 0; j < 6; j++) {
+      P[i][j] = FPFt[i][j] + Q[i][j];
+    }
+  }
 }
