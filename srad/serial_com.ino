a4398d64c2e021c0255349fdb6a2723799df6916
treecaptcha <treecaptcha@treecaptcha.net>
2025-02-01 13:57:57 -0500

    log and send over serial

--- srad/serial_com.ino
+++ srad/serial_com.ino
@@ -1,9 +1,20 @@
 #ifdef SERIALCOM
-void send(char* data){
+void send(char* data, int length){
+  char x;
 
+  for (int i = 0; i < length; i++){
+
+    x = data[i];
+    if ( (x>='0' && x<='9')? (x-'0'):((x>='A' && x<= 'F')?((x-'A')+10):(0)) ){
+      Serial.write(x);
+    }
+    Serial.write("\n");
 }
-void print(){
-  
+}
+void log(String data){
+  Serial.write("02");
+  Serial.write(data);
+  Serial.write("\n");
 }
 void doComReady(){
   Serial.begin(115200);
