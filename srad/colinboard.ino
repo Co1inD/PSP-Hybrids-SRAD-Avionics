39381e1b021c5a66f5f433c27cd17565b01a881f
treecaptcha <treecaptcha@treecaptcha.net>
2025-02-01 13:57:38 -0500

    use log

--- srad/colinboard.ino
+++ srad/colinboard.ino
@@ -18,12 +18,12 @@ void doSetupBaro(){
     // You can also pass in a Wire library object like &Wire2
     // status = bme.begin(0x76, &Wire2)
     if (!status) {
-        Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
-        Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
-        Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
-        Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
-        Serial.print("        ID of 0x60 represents a BME 280.\n");
-        Serial.print("        ID of 0x61 represents a BME 680.\n");
+        log("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
+        log("SensorID was: 0x"); log(bme.sensorID(),16);
+        log("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+        log("   ID of 0x56-0x58 represents a BMP 280,\n");
+        log("        ID of 0x60 represents a BME 280.\n");
+        log("        ID of 0x61 represents a BME 680.\n");
         while (1) delay(10);
     }
 }
