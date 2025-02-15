84cb73672213c1a645a4d011283f003179a2877a
JGraff24 <graff12@purdue.edu>
2025-02-15 15:45:20 -0500

    Update SD_log.ino

--- srad/SD_log.ino
+++ srad/SD_log.ino
@@ -1,13 +1,22 @@
 #ifdef SD_Log
-void doLogReady(){
-  // some stuff
+#include <SPI.h>
+#include <SD.h>
+void doLogReady()
+{
+  File myFILE;
+  myFile = SD.open("Output_File.txt", FILE_APPEND);
+  myFile.write("Test Data\n");
+  myFile = SD.close();
 }
 void log(char* data, int length){
   // some stuff
+  myFile = SD.open("Output_File.txt", FILE_APPEND);
+  //logging data
   free(data);
 }
 void log(String data){
-  // some stuff
+  myFile = SD.open("Output_File.txt", FILE_APPEND);
+  //logging data
   free(data);
 }
 #endif
