dd3eb07535ec9aa6cb1775337f2c5efbd0ebe59a
treecaptcha <treecaptcha@treecaptcha.net>
2025-02-15 19:41:54 -0500

    Add threading

--- srad/srad.ino
+++ srad/srad.ino
@@ -6,13 +6,49 @@
 // Logging Library: SD_Log | Null_Log
 #define Null_Log 1
 
+#define UPDATE_PACKET_LENGTH 50
 
 #define G 9.8               // m/s/s
 #define SPEED_OF_SOUND 300  // m/s
+
+TaskHandle_t mainTask;
+TaskHandle_t logTask;
+TaskHandle_t comTask;
+
+
+
 void setup() {
   doSensorReady();
   doComReady();
   doLogReady();
+  xTaskCreatePinnedToCore(
+      mainLoop, /* Function to implement the task */
+      "main loop", /* Name of the task */
+      1000,  /* Stack size in words */
+      NULL,  /* Task input parameter */
+      0,  /* Priority of the task */
+      &mainTask,  /* Task handle. */
+      1); /* Core where the task should run */
+  xTaskCreatePinnedToCore(
+      logLoop, /* Function to implement the task */
+      "log loop", /* Name of the task */
+      1000,  /* Stack size in words */
+      NULL,  /* Task input parameter */
+      1,  /* Priority of the task */
+      &logTask,  /* Task handle. */
+      0); /* Core where the task should run */
+  xTaskCreatePinnedToCore(
+      sendLoop, /* Function to implement the task */
+      "send loop", /* Name of the task */
+      1000,  /* Stack size in words */
+      NULL,  /* Task input parameter */
+      1,  /* Priority of the task */
+      &comTask,  /* Task handle. */
+      0); /* Core where the task should run */
+}
+
+void loop(){
+  delay(1000);
 }
 
 int armed, takenOff, boostDone, sonicOver, paraOneLaunch, paraTwoLaunch;
@@ -24,6 +60,43 @@ float* newAlt[10];
 int altIndex;
 bool altFull;
 
+char* logData[3000];
+char* sendData[3000];
+int lengths[3000];
+int IDX = 0;
+
+void logLoop(void *parameter){
+  int loggedIDX = 0;
+  while(true){
+    while(IDX != loggedIDX){
+      log(logData[loggedIDX], lengths[loggedIDX]);
+      loggedIDX++;
+      if (loggedIDX == 3000) loggedIDX = 0;
+    }
+    delay(10);
+  }
+}
+void sendLoop(void *parameter){
+  int sentIDX = 0;
+  while(true){
+    while(IDX != sentIDX){
+      log(logData[sentIDX], lengths[sentIDX]);
+      sentIDX++;
+      if (sentIDX == 3000) sentIDX = 0;
+    }
+    delay(10);
+  }
+}
+
+void queueRecord(char* data, int length){
+  char* copiedData = (char*)malloc(length);
+  memcpy(copiedData, data, length);
+  lengths[IDX] = length;
+  logData[IDX] = data;  
+  sendData[IDX] = copiedData; 
+  IDX++;
+
+}
 
 void loop() {
   update();
@@ -70,6 +143,72 @@ void loop() {
       launchChute(2);
     }
   }
+void queueRecord(String data){
+  const char* cstr = data.c_str();
+  int len = 0;
+  while(!cstr[len++]);
+  char* copiedcstr = (char*)malloc(len);
+  memcpy(copiedcstr, cstr, len);
+  queueRecord(copiedcstr, len);
+}
+
+void mainLoop(void *parameter) {
+  while(true){
+    int startTime = millis();
+    update();
+    float vel = getVelocityMagnitude();
+    float acc = getAccelerationMagnitude();
+    float alt = getBaro();
+    if (!armed) return;
+    if (!takenOff) {
+      if (acc > 3 * G) {
+        takenOff = millis();
+      }
+      return;
+    }
+    if (!boostDone) {
+      float* accVec = getAcceleration();
+      if (accVec[2] < 0) {
+        boostDone = millis();
+      }
+      return;
+    }
+    if (!sonicOver) {
+      if (vel < SPEED_OF_SOUND) {
+        sonicOver = millis();
+      }
+      return;
+    }
+    if (!paraOneLaunch) {
+      if (altIndex == 10) {
+        altIndex = 0;
+        altFull = millis();
+      }
+      oldAlt[altIndex] = newAlt[altIndex];
+      newAlt[altIndex] = alt;
+      altIndex++;
+      if (averageFloat(oldAlt, 10) >= averageFloat(newAlt, 10)) {
+        paraOneLaunch = millis();
+        launchChute(1);
+      }
+    }
+    if(!paraTwoLaunch){
+      if (alt < paraTwoAlt){
+        paraTwoLaunch = millis();
+        launchChute(2);
+      }
+    }
+    int endTime = millis();
+    delay(startTime > endTime - 10? 10 - (endTime - startTime ): 0);
+  }
+}
+
+float averageFloat(float* arr , int len){
+  float sum = 0.0f;
+  for(int i = 0; i < len; i++){
+    sum += arr[i];
+  }
+  return sum/len;
 }
 
 float getAccelerationMagnitude() {
